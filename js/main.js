b13ce6e697ecb6a2f2ca5692efe061f5802e4c11
ronnie-lake <darya8levchuk@gmail.com>
2017-03-26 17:50:53 +0300

    added js, fixed less

---
+++ js/main.js
@@ -0,0 +1,42 @@
+
+$(document).ready(function() {
+	$('select').selectric();
+
+	$('.header__hidden-menu').click(function() {
+		$('.hidden-menu').toggleClass('hidden-menu__active');
+	});
+
+	$(document).click(function(e) {
+		 if(!$(e.target).is('.header__hidden-menu'))  {
+		 	$('.hidden-menu__active').removeClass('hidden-menu__active');
+		 } 
+	});
+
+
+	$('[id=slides]').on('click', '.active', function(e){
+		if($(e.target).is('a')) return;
+		var parent = $(this).parents('#viewport');	
+		if($(this).is(':last-child')) {
+			$(this).fadeOut(0).removeClass('active');
+			parent.find('#slides li:first-child').fadeIn(300).addClass('active');
+			parent.find('#navigation .active').removeClass('active');
+			parent.find('#navigation li:first-child').addClass('active');
+		} else {
+			var active = parent.find('#navigation .active');
+			$(this).fadeOut(0).removeClass('active');
+			$(this).next().fadeIn(300).addClass('active');
+			active.removeClass('active');
+			active.next().addClass('active');
+		}
+	})
+
+	$('[id=navigation] li').click(function(){
+		var parent = $(this).parents('#viewport');
+		var num = $(this).index() + 1;
+		$(this).parent().find('.active').removeClass('active');
+		$(this).addClass('active');
+		parent.find('#slides .active').fadeOut(0).removeClass('active');
+		parent.find('#slides li:nth-child('+ num +')').fadeIn(300).addClass('active');
+		
+	})
+})
